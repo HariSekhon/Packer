15ca7e6040096dafde76872d914c0d9be181a3ae
Hari Sekhon <harisekhon@gmail.com>
2023-06-02 05:28:43 +0100

    added local.sh

---
+++ scripts/local.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2023-06-02 05:26:55 +0100 (Fri, 02 Jun 2023)
+#
+#  https://github.com/HariSekhon/Packer-templates
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+echo "Packer Environment Variables:"
+echo
+env | grep PACKER || :
+echo
+echo "Creating ~/vboxsf"
+mkdir -p -v ~/vboxsf
+echo
+echo "Adding Shared Folder to VM"
+VBoxManage sharedfolder add "$PACKER_BUILD_NAME" --name vboxsf --hostpath ~/vboxsf --automount --transient
