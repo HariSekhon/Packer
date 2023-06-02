e3ae99bb40f77e6a114161957f1662c84498395c
Hari Sekhon <harisekhon@gmail.com>
2023-06-02 05:13:18 +0100

    added vboxsf.sh

---
+++ scripts/vboxsf.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2023-06-02 05:11:49 +0100 (Fri, 02 Jun 2023)
+#
+#  https://github.com/HariSekhon/Packer-templates
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+sudo=""
+if [ "$EUID" -ne 0 ]; then
+    sudo=sudo
+fi
+
+dir="/mnt/vboxsf"
+
+$sudo mkdir -pv "$dir"
+echo "Mounting $dir"
+$sudo mount -t vboxsf vboxsf "$dir"
