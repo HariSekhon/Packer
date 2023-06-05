52941f039b271d11ad66351208fc615451bd1177
Hari Sekhon <harisekhon@gmail.com>
2023-06-05 18:21:13 +0100

    added prepare-ubuntu-22.04.sh

---
+++ scripts/prepare-ubuntu-22.04.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2023-06-03 02:04:01 +0100 (Sat, 03 Jun 2023)
+#
+#  https://github.com/HariSekhon/Packer-templates
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# Downloads the Ubuntu ISO and generates an ISO with the Ubuntu AutoInstaller configs on which to boot the tart
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# https://ubuntu.com/download/server/arm
+"$srcdir/prepare-ubuntu.sh" "ubuntu-22.04.2-live-server-arm64.iso"
