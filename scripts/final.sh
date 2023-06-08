92f75e550af43a420a509975d3bd95f04d6e57fe
Hari Sekhon <harisekhon@gmail.com>
2023-06-08 02:31:27 +0100

    added final.sh

---
+++ scripts/final.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2023-06-08 02:28:00 +0100 (Thu, 08 Jun 2023)
+#
+#  https://github.com/HariSekhon/Packer-templates
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/HariSekhon
+#
+
+# ============================================================================ #
+#               P a c k e r   F i n a l   S h e l l   S c r i p t
+# ============================================================================ #
+
+# Use to clean up caches, lock out SSH password auth and other cleanup actions
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+#srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+apt-get clean
+
+# SSH password auth is needed for Packer, but after build disable this
+sed -i '/PasswordAuthentication.*yes/PasswordAuthentication no/' /etc/ssh/sshd_config
